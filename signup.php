2191577891621608f01fc33ccf8c3fb3d37d53c7
Phillip <pwg1994@gmail.com>
2016-10-20 12:23:34 -0400

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+		<head>
+    
+    
+        </head>
+    
+    <body>
+		<div>
+			  <form action="" method="post">
+
+            <p><input id="email" name="email" type="text" placeholder="Email"></p>
+            </form>
+			<p>
+				Results and page (C) Copyright GeekLove Inc.
+			</p>
+			
+			<ul>
+				<li>
+					<a rel="noopener" href="index.php">
+						<img src="back.gif" alt="icon"/>
+						Back to front page
+					</a>
+				</li>
+			</ul>
+		</div>
+
+    </body>
+</html>
